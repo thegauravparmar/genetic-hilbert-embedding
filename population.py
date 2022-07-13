bcbe16aaead8fa754fff2832dbab1c625d0048e1
H4ckOm <gparmar1522@gmail.com>
2022-07-13 14:09:53 +0530

    feat: init pop

--- population.py
+++ population.py
@@ -2,23 +2,7 @@ import random
 import conversions
 
 
-def int2binary9(n):
-    bb = ""
-    tmp = n
-    while(tmp > 0):
-        if(tmp % 2 == 1):
-            bb += "1"
-        else:
-            bb += "0"
-        tmp = tmp//2
-    while(len(bb) < 9):
-        bb += "0"
-    tt = bb[::-1]
-    return tt
-
-
 def genPopulation(iter):
-    # import conversions
 
     population = []*iter
     for i in range(iter):
@@ -26,9 +10,10 @@ def genPopulation(iter):
         yOffset = random.randrange(0, 512)
         sbPole = random.randrange(0, 2)
         sbDir = random.randrange(0, 2)
-        # xOffsetBin = conversions.int2binary9(xOffset)
-        # yOffsetBin = conversions.int2binary9(yOffset)
-        finalChrome = str(sbDir) + str(sbPole)
+        xOffsetBin = conversions.int2binary9(xOffset)
+        yOffsetBin = conversions.int2binary9(yOffset)
+        finalChrome = str(sbDir) + str(sbPole) + \
+            str(yOffsetBin) + str(xOffsetBin)
         population.append(finalChrome)
 
     return population
